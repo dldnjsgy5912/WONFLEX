0b77df9e6a4877c8d9e7cffe85af937e97714a07
dldnjsgy5912 <dldnjsgy5912@naver.com>
2023-01-02 01:43:45 +0900

    feat : Header 초기구성

---
+++ src/Component/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="con-title">
+      <img
+        src="logo_transparent"
+        alt=""
+        onClick={() => {
+          navigate('/');
+        }}
+        width="100%"
+        className="title"
+      />
+    </div>
+  );
+};
+
+export default Header;
